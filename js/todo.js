11ceeaabba949781a06456db2a2143be24dc37e7
bradwilson <dotnetguy@gmail.com>
2012-06-15 21:13:29 -0700

    Step 2

---
+++ js/todo.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="jquery-1.7.2.js" />
+/// <reference path="knockout-2.1.0.js" />
+
+function ToDoViewModel() {
+    var self = this;
+
+    function ToDoItem(root, title, finished) {
+        var self = this;
+
+        self.title = ko.observable(title);
+        self.finished = ko.observable(finished);
+
+        self.remove = function () {
+            root.items.remove(self);
+        };
+    };
+
+    self.addItemTitle = ko.observable("");
+    self.items = ko.observableArray([
+        new ToDoItem(self, "Spread the word about ASP.NET Web API", false),
+        new ToDoItem(self, "Wash the car", false),
+        new ToDoItem(self, "Get a haircut", true)
+    ]);
+
+    self.add = function () {
+        self.items.push(new ToDoItem(self, self.addItemTitle(), false));
+        self.addItemTitle("");
+    };
+};
+
+$(function () {
+    var viewModel = new ToDoViewModel();
+
+    ko.applyBindings(viewModel);
+});
